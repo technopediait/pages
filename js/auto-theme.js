dbaea54552a5015787ef4f66fd84f4f3f73452c5
technopediait <mohammadshayly77@gmail.com>
2023-11-02 06:41:45 +0330

    23

--- js/auto-theme.js
+++ js/auto-theme.js
@@ -1,43 +1,33 @@
+function sendNotification() {
+  // ابتدا بررسی کنید که آیا مرورگر از API Notifications پشتیبانی می‌کند
+  if (Notification.permission === "granted") {
+    // یک نوتیفیکیشن جدید ایجاد کنید
+    const notification = new Notification("عنوان نوتیفیکیشن", {
+      // محتوای نوتیفیکیشن
+      body: "متن نوتیفیکیشن",
+      // آیکون نوتیفیکیشن
+      icon: "https://example.com/icon.png",
+      // دکمه‌های نوتیفیکیشن
+      buttons: [
+        {
+          // متن دکمه
+          label: "تایید",
+          // عملکرد دکمه
+          onClick: () => console.log("تایید شد"),
+        },
+        {
+          // متن دکمه
+          label: "لغو",
+          // عملکرد دکمه
+          onClick: () => console.log("لغو شد"),
+        },
+      ],
+    });
 
-    // تابعی برای تعیین حالت روشن یا تاریک بر اساس وضعیت سیستم
-    function getMode() {
-        // اگر دستگاه اندروید است، حالت سیستم را تعیین کنید
-        if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-          return "dark";
-        } else {
-          return "light";
-        }
-      
-      
-      // تابعی برای تغییر حالت صفحه
-      function changeMode(mode) {
-        // حالت صفحه را بر اساس مقدار بازگشتی تابع getMode() تغییر دهید
-        const body = document.querySelector("body");
-        body.classList.remove("light", "dark");
-        body.classList.add(mode);
-      }
-        // رنگ پس زمینه را تغییر دهید
-        if (mode === "dark") {
-          body.style.backgroundColor = "#000";
-        } else {
-          body.style.backgroundColor = "#ffffff";
-        }
-      
-        // رنگ متن را تغییر دهید
-        if (mode === "dark") {
-          body.style.color = "#fff";
-        } else {
-          body.style.color = "#000";
-        }
-      }
-      
-      // تعیین حالت اولیه صفحه
-      const mode = getMode();
-      changeMode(mode);
-      
-      // نظارت بر تغییر حالت سیستم و تغییر حالت صفحه به صورت خودکار
-      window.addEventListener("color-scheme-change", () => {
-        const mode = getMode();
-        changeMode(mode);
-      });
-      
+    // نوتیفیکیشن را نمایش دهید
+    notification.show();
+  } else {
+    // از کاربر اجازه بگیرید
+    Notification.requestPermission();
+  }
+}
