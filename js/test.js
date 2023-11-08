06f88b2f450188f9569e8b97e65d38e32928b96f
technopediait <mohammadshayly77@gmail.com>
2023-11-08 21:53:18 +0330

    tocen

--- js/test.js
+++ js/test.js
@@ -1,30 +1,28 @@
+//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
 
+const axios = require("axios");
 
-// جمع‌آوری اطلاعات فرم
-const data = {
-  filename: document.querySelector("input[name='filename']").value,
-  content: document.querySelector("textarea[name='content']").value,
-  type: document.querySelector("select[name='type']").value,
+const updateTag = async (token, repo, filename, tag, newContent) => {
+  const url = `https://api.github.com/repos/${repo}/contents/${filename}`;
+  const headers = {
+    "Authorization": `Bearer ${token}`,
+  };
+  const data = `<${tag}>${newContent}</${tag}>`;
+
+  const response = await axios.put(url, headers, data);
+  return response.status === 200;
 };
 
-// ارسال اطلاعات به مخزن گیت هاب
-axios({
-  method: "POST",
-  url: "https://api.github.com/repos/technopediait/pages/contents/[filename]",
-  headers: {
-    Authorization: `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`,
-  },
-  data: data,
-})
-  .then((response) => {
-    // موفقیت
-    console.log(response);
-  })
-  .catch((error) => {
-    // خطا
-    console.log(error);
-  });
+const token = "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";
+const repo = "technopediait/pages";
+const filename = "index.html";
+const tag = "h1";
+const newContent = "This is a new heading";
 
+const success = await updateTag(token, repo, filename, tag, newContent);
 
-
- 
+if (success) {
+  console.log("تگ HTML با موفقیت بروزرسانی شد.");
+} else {
+  console.log("خطایی در بروزرسانی تگ HTML رخ داد.");
+}
