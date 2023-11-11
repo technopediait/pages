faaa94f336b8f27b0334b4e0967797752160ecce
تکنو پدیا <149099729+technopediait@users.noreply.github.com>
2023-11-11 07:00:16 +0330

    Update test.js

--- js/test.js
+++ js/test.js
@@ -42,12 +42,7 @@ function addOutput() {
 
 var inpP = document.createElement("b") ;
 inpP.textContent =  inputText ;
-     window.scrollBy(0, window.innerHeight); // می‌توانید مقدار انتخابی برای اسکرول را تنظیم کنید
-
-
-// تایمر برای اجرای تابع به صورت مکرر
-var scrollInterval = setInterval(scrollDown, 1000); // هر 1000 میلی‌ثانیه یکبار اسکرول به پایین انجام می‌شود
-
+     
 out.appendChild(inpP) ;
 out.appendChild(outTXT) ;
 
