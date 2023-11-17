45c0e7b818e15ef1b0738375ddee2c847570cb64
technopediait <mohammadshayly77@gmail.com>
2023-11-17 09:02:14 +0330

    fixed the footer

--- js/script.js
+++ js/script.js
@@ -145,8 +145,74 @@ serchIcon.addEventListener("click", function() {
   result.classList.add("on-out") ;
   input.classList.remove("none") ;
   result.classList.remove("none-out") ;
-  searchImg.src = "./icon/close.svg"
-  searchImg.style.transform = "rotate(180deg)"
+  searchImg.src = "./icon/close.svg" ;
+  searchImg.style.transform = "rotate(180deg)" ;
   }
 })
 
+const  FCMTImg1 = document.getElementById("fcmt-img1")
+const  FCMTImg2 = document.getElementById("fcmt-img2")
+const  FCMTImg3 = document.getElementById("fcmt-img3")
+const  FCMTImg4 = document.getElementById("fcmt-img4")
+
+const FCMT1 = document.getElementById("fcmt1")
+const FCMT2 = document.getElementById("fcmt2")
+const FCMT3 = document.getElementById("fcmt3")
+const FCMT4 = document.getElementById("fcmt4")
+
+const FCMI1 = document.getElementById("fcmi1")
+const FCMI2 = document.getElementById("fcmi2")
+const FCMI3 = document.getElementById("fcmi3")
+const FCMI4 = document.getElementById("fcmi4")
+
+FCMTImg1.addEventListener("click", () => {
+  if (FCMI1.style.height === "auto"){
+    FCMI1.style.height = "0"
+    FCMTImg1.style.transform = "rotate(90deg)"
+    
+  }
+
+  else {
+    FCMI1.style.height = "auto"
+    FCMTImg1.style.transform = "rotate(0deg)"
+  }
+})
+
+FCMTImg2.addEventListener("click", () => {
+  if (FCMI2.style.height === "auto"){
+    FCMI2.style.height = "0"
+    FCMTImg2.style.transform = "rotate(90deg)"
+    
+  }
+
+  else {
+    FCMI2.style.height = "auto"
+    FCMTImg2.style.transform = "rotate(0deg)"
+  }
+})
+
+FCMTImg3.addEventListener("click", () => {
+  if (FCMI3.style.height === "auto"){
+    FCMI3.style.height = "0"
+    FCMTImg3.style.transform = "rotate(90deg)"
+    
+  }
+
+  else {
+    FCMI3.style.height = "auto"
+    FCMTImg3.style.transform = "rotate(0deg)"
+  }
+})
+
+FCMTImg4.addEventListener("click", () => {
+  if (FCMI4.style.height === "auto"){
+    FCMI4.style.height = "0"
+    FCMTImg4.style.transform = "rotate(90deg)"
+    
+  }
+
+  else {
+    FCMI4.style.height = "auto"
+    FCMTImg4.style.transform = "rotate(0deg)"
+  }
+})
