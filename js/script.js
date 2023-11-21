fcd804e3530007efbb7d6defd048d8bfc6fba5be
technopediait <mohammadshayly77@gmail.com>
2023-11-21 06:40:00 +0330

    11111

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+
+const progressBar = document.querySelector(".progress-bar");
+const height =
+  document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.addEventListener("scroll", () => {
+  const scrollTop = document.documentElement.scrollTop;
+  const scrolled = (scrollTop / height) * 100;
+
+  progressBar.style.width = `${scrolled}%`;
+});
+
 let slideIndex = 1;
 
 function setSlide(input,index){
