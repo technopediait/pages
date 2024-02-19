92071a7da3f664c9a174f38fffdc41869f45c81f
technopediait <mohammadshayly77@gmail.com>
2024-02-19 07:45:35 +0330

    bbbb

---
+++ l.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    
+    int x = 1;
+    int y = 9;
+    
+    if(x >= 1 && y <= 8 ){
+      Console.WriteLine("Happy");  
+      }else{
+      Console.WriteLine("unHappy ");
+      }
+    }
+  }
+}
