9ad9ef4b9e23fa707828a4079c9c69be0bb579e8
technopediait <mohammadshayly77@gmail.com>
2024-03-19 17:24:57 +0330

    121212

--- theme/js/admin.js
+++ theme/js/admin.js
@@ -21,82 +21,34 @@ sidebarBtn.addEventListener("click" ,  function(){
   }
 });
 
-var title1 = document.getElementById("title1");
-var title2 = document.getElementById("title2");
-var title3 = document.getElementById("title3");
-var title4 = document.getElementById("title4");
 
-var titleIcon1 = document.getElementById("title-icon1");
-var titleIcon2 = document.getElementById("title-icon2");
-var titleIcon3 = document.getElementById("title-icon3");
-var titleIcon4 = document.getElementById("title-icon4");
+var elements = document.querySelectorAll('.title, .title-icon, .item');
 
-var item1 = document.getElementById("item1");
-var item1 = document.getElementById("item1");
-var item2 = document.getElementById("item2");
-var item3 = document.getElementById("item3");
-var item4 = document.getElementById("item4");
+elements.forEach(function(element) {
+  if (element.classList.contains('title')) {
+    var title = element;
+    var id = title.id.slice(-1);
+    var icon = document.querySelector('.title-icon' + id);
+    var item = document.querySelector('.item' + id);
 
-title1.addEventListener("click" ,  function(){
-  if( item1.style.height === "auto" ){
-    item1.style.height = "0" ;
-    titleIcon1.style.transform = "rotate(90deg)" ;
- }else{
-  item1.style.height = "auto" ;
-  titleIcon1.style.transform = "rotate(0deg)" ;
-  item2.style.height = "0" ;
-  item3.style.height = "0" ;
-  item4.style.height = "0" ;
-  titleIcon2.style.transform = "rotate(90deg)" ;
-  titleIcon3.style.transform = "rotate(90deg)" ;
-  titleIcon4.style.transform = "rotate(90deg)" ;
- }
+    title.addEventListener("click", function() {
+      if (item.style.height === "auto") {
+        item.style.height = "0";
+        icon.style.transform = "rotate(90deg)";
+      } else {
+        item.style.height = "auto";
+        icon.style.transform = "rotate(0deg)";
+        elements.forEach(function(otherElement) {
+          if (otherElement !== element && otherElement.classList.contains('item')) {
+            otherElement.style.height = "0";
+            var otherIcon = document.querySelector('.title-icon' + otherElement.id.slice(-1));
+            otherIcon.style.transform = "rotate(90deg)";
+          }
+        });
+      }
+    });
+  }
 });
 
-title2.addEventListener("click" ,  function(){
-  if( item2.style.height === "auto" ){
-    item2.style.height = "0" ;
-    titleIcon2.style.transform = "rotate(90deg)" ;
- }else{
-  item2.style.height = "auto" ;
-  titleIcon2.style.transform = "rotate(0deg)" ;
-  item1.style.height = "0" ;
-  item3.style.height = "0" ;
-  item4.style.height = "0" ;
-  titleIcon1.style.transform = "rotate(90deg)" ;
-  titleIcon3.style.transform = "rotate(90deg)" ;
-  titleIcon4.style.transform = "rotate(90deg)" ;
- }
-});
 
-title3.addEventListener("click" ,  function(){
-  if( item3.style.height === "auto" ){
-    item3.style.height = "0" ;
-    titleIcon3.style.transform = "rotate(90deg)" ;
- }else{
-  item3.style.height = "auto" ;
-  titleIcon3.style.transform = "rotate(0deg)" ;
-  item2.style.height = "0" ;
-  item1.style.height = "0" ;
-  item4.style.height = "0" ;
-  titleIcon2.style.transform = "rotate(90deg)" ;
-  titleIcon1.style.transform = "rotate(90deg)" ;
-  titleIcon4.style.transform = "rotate(90deg)" ;
- }
-});
 
-title4.addEventListener("click" ,  function(){
-  if( item4.style.height === "auto" ){
-    item4.style.height = "0" ;
-    titleIcon4.style.transform = "rotate(90deg)" ;
- }else{
-  item4.style.height = "auto" ;
-  titleIcon4.style.transform = "rotate(0deg)" ;
-  item2.style.height = "0" ;
-  item3.style.height = "0" ;
-  item1.style.height = "0" ;
-  titleIcon2.style.transform = "rotate(90deg)" ;
-  titleIcon3.style.transform = "rotate(90deg)" ;
-  titleIcon1.style.transform = "rotate(90deg)" ;
- }
-});
